4e4a244a73a0916d2861cde5ae61e1c1b3b23719
catureba <catureba>
2022-10-11 19:41:43 -0300

    Trade generated value IDENTITY for  generated value SEQUENCE

--- src/main/java/com/example/apibirds/models/Bird.java
+++ src/main/java/com/example/apibirds/models/Bird.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 public class Bird implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
     private Long id;
     private String nome;
     private String image;
